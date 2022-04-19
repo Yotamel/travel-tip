5736dae7a11aa1352f6fe2787e2c4228ebf5c782
Eviak <karbiane2@gmail.com>
2022-04-19 20:50:22 +0300

    selectedLocation updates now on map click

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -9,21 +9,27 @@ export const mapService = {
 }
 
 
- var gLatLng = {lat: 10, lng: 10}
+var gSelectedLocation = {
+    pos: { lat: null, lng: null },
+    name,
+}
 var gMap;
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap');
     return _connectGoogleApi()
         .then(() => {
-            console.log('google available');
             gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                    center: { lat, lng },
-                    zoom: 15
-                })
-            gMap.addListener('click', () => {})
-            console.log('Map!', gMap);
+                    document.querySelector('#map'), {
+                        center: { lat, lng },
+                        zoom: 15
+                    })
+            gMap.addListener('click', (mapsMouseEvent) => {
+            const clickedLatLng = mapsMouseEvent.latLng.toJSON()
+            gSelectedLocation.pos = clickedLatLng
+                getGeoLocation(`latlng=${clickedLatLng.lat},${clickedLatLng.lng}`)
+                     .then(res => {gSelectedLocation.name = res.results[0].formatted_address
+                    })
+            })
         })
 }
 
@@ -41,11 +47,10 @@ function panTo(lat, lng) {
     gMap.panTo(laLatLng);
 }
 
-function searchByAddress(address){
+function searchByAddress(address) {
     if (!address) return
-    const geoAddress = 'address=' +address.split(' ').join('+')
+    const geoAddress = 'address=' + address.split(' ').join('+')
     getGeoLocation(geoAddress)
-        // .then(res => )
 }
 
 function _connectGoogleApi() {
